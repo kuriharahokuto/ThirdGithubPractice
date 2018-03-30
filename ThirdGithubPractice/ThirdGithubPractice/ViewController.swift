b979f25832fac486bd01b26681ca8d089139d581
栗原北斗 <martnight1011@gmail.com>
2018-03-30 15:57:31 +0900

    新規プロジェクトを追加

---
+++ ThirdGithubPractice/ThirdGithubPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThirdGithubPractice
+//
+//  Created by 栗原北斗 on 2018/03/30.
+//  Copyright © 2018年 栗原北斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
